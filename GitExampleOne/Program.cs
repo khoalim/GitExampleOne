c1f999a63361db8c26a2611f5ddfc086e3d34f3f
Kim Hoalim <kthoalim@gmail.com>
2019-09-04 11:41:57 -0400

    First Commit

---
+++ GitExampleOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GitExampleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Kimberly";
+
+            string lastName = "Ho-A-Lim";
+
+            int age = 29;
+
+            int puttingOnSomeYears = 40;
+
+            int oldAge = age + puttingOnSomeYears;
+
+            Console.WriteLine(firstName + " " + lastName); // Adding the " " gives a space btwn the names
+     
+            Console.WriteLine("I am " +  age + " years old");
+            
+            Console.WriteLine("I'm really glad I'm not " + oldAge + " years old");
+
+
+        }
+    }
+}
