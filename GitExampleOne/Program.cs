690c6fdfa5502270e3364decabb4b425b6064c56
Kim Hoalim <kthoalim@gmail.com>
2019-09-04 12:03:48 -0400

    Add new variable

--- GitExampleOne/Program.cs
+++ GitExampleOne/Program.cs
@@ -11,6 +11,8 @@ namespace GitExampleOne
 
             string lastName = "Ho-A-Lim";
 
+            string sweetness = "Not Brett";
+
             int age = 29;
 
             int puttingOnSomeYears = 40;
@@ -19,9 +21,11 @@ namespace GitExampleOne
 
             Console.WriteLine(firstName + " " + lastName); // Adding the " " gives a space btwn the names
      
-            Console.WriteLine("I am " +  age + " years old");
+            Console.WriteLine("I am " +  age + " years old"); // Put space btwn last or first letters and " to add space
             
             Console.WriteLine("I'm really glad I'm not " + oldAge + " years old");
+
+            Console.WriteLine("Who is the sweetness? " + sweetness);
 
 
         }
